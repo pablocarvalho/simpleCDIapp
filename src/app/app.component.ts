1e7be7012e171f61ba6fb673bd4ea197e25cd31a
pablocarvalho <pablomccarvalho@gmail.com>
2020-11-08 14:23:17 -0300

    learning about charts, just commiting a dummmy chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'simpleCDIapp';
+export class AppComponent implements OnInit {
+    @ViewChild("meuCanvas", { static: true } ) elemento: ElementRef;
+    
+    ngOnInit(){
+        new Chart(this.elemento.nativeElement,{
+            type: 'line',
+            data:{
+                labels: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+                datasets:[
+                    {
+                        data:[85,73,86,81,84,86,94,60,62,65,41,58]
+                    }
+                ]
+            }
+
+    });
+  }
 }
