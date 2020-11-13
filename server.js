e58a43e07a9b41863c1e876b6673e123e299006c
pablocarvalho <pablomccarvalho@gmail.com>
2020-11-13 15:15:06 -0300

    trying to fix blank screen problem

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 app.listen(process.env.PORT || 4200);
 
-app.use(express.static(path.join(__dirname + '/dist/simpleCDIapp/index.html')));
+app.use(express.static(path.join(__dirname,'/dist/simpleCDIapp')));
 
 app.get('/*',function(req,res){    
-    res.sendFile(path.join(__dirname+'/dist/simpleCDIapp/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/simpleCDIapp/index.html'));
 })
 
 console.log('Console listening!');
