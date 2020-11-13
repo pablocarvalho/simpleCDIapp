44e39e5ebe1db9841459142f03e7b7a10c753588
pablocarvalho <pablomccarvalho@gmail.com>
2020-11-13 11:24:58 -0300

    trying to fix index.html problem

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 app.listen(process.env.PORT || 4200);
 
-app.use(express.static(__dirname + '/dist/cdi-app-front/index.html'));
+app.use(express.static(__dirname + '/dist/simpleCDIapp/index.html'));
 
 app.get('/*',function(req,res){    
-    res.sendFile(path.join(__dirname+'/dist/cdi-app-front/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/simpleCDIapp/index.html'));
 })
 
 console.log('Console listening!');
